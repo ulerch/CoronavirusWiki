c384a01f164f4156b57cede5f13dcafceeb591c3
Urs Lerch <mail@ulerch.net>
2020-03-24 18:59:30 +0100

    adding icon to list items header

--- static/wiki.js
+++ static/wiki.js
@@ -5,6 +5,9 @@ $( document ).ready(function() {
   $('#createEntry').on('click tap', function(event) {
     window.location.href = '/create-' + event.target.attributes.row.value + '-' + event.target.attributes.col.value + '/';
   });
+  $(".card .btn-link").click( function() {
+    $(this).find('span.glyphicon').toggleClass('glyphicon-chevron-down glyphicon-chevron-up');
+  });
   $('#id_text').attr('rows', 5);
   $('#id_title, #id_text, #id_name, #id_email').parents('tr').css({'visibility': 'visible', 'height': 0});
 });
